fe48f2f5cf7a8e68d1ecce08963578912232395e
switch-coder <sang_hk@naver.com>
2021-09-09 00:26:26 +0900

    board crud learn

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+
+    constructor(private boardsService: BoardsService) { }
+
+    @Get('/')
+    getAllBoard(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto)
+
+    }
+}
