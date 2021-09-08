9121e00f2969e41f3a070d4096e828d83e3ea414
switch-coder <sanghyuk@kinterch.com>
2021-09-09 00:25:59 +0900

    nest cli

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsService } from './boards/boards.service';
+@Module({
+  imports: [BoardsModule],
+  providers: [BoardsService],
+
+})
+export class AppModule { }
