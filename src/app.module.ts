9971c7b34f8cef8e1e55a0e0af9033edf899ac60
switch-coder <sang_hk@naver.com>
2021-09-10 00:21:15 +0900

    repository 생성 mariadb에 데이터 저장

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { BoardsModule } from './boards/boards.module';
-import { BoardsService } from './boards/boards.service';
 import { typeORMConfig } from './configs/typeorm.config';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule
   ],
-  providers: [BoardsService],
+  providers: [],
 
 })
 export class AppModule { }
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-vaildation.pipe';
@@ -9,34 +9,37 @@ export class BoardsController {
 
     constructor(private boardsService: BoardsService) { }
 
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards();
-    }
+
+    
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards();
+    // }
 
     @Post()
     @UsePipes(ValidationPipe)
     createBoard(
         @Body() createBoardDto: CreateBoardDto
-    ): Board {
+    ): Promise<Board> {
         return this.boardsService.createBoard(createBoardDto)
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsService.getBoardById(id)
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
     }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        this.boardsService.deleteBoard(id);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void {
+    //     this.boardsService.deleteBoard(id);
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ) {
-        return this.boardsService.updateBoardStatus(id, status)
-    }
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    // ) {
+    //     return this.boardsService.updateBoardStatus(id, status)
+    // }
+
 }
